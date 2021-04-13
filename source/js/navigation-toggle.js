758ff49d037332333d936265a302d20b96dba335
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-04-13 23:04:15 +0300

    module3-task1

---
+++ source/js/navigation-toggle.js
@@ -0,0 +1,11 @@
+const navigationToggle = document.querySelector('.page-header__nav-toggle');
+console.log(navigationToggle)
+const userNavigation = document.querySelector('.user-nav');
+const mainNavigation = document.querySelector('.main-nav');
+
+navigationToggle.addEventListener('click', function (evn) {
+  evn.preventDefault();
+  navigationToggle.classList.toggle('page-header__nav-toggle--active');
+  userNavigation.classList.toggle('user-nav--active');
+  mainNavigation.classList.toggle('main-nav--active');
+});
