4807b41030d81debee3abccab4938489abeaaa16
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-05-03 23:16:31 +0300

    module4-task1

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,12 @@
 const navigationToggle = document.querySelector('.page-header__nav-toggle');
-console.log(navigationToggle)
 const userNavigation = document.querySelector('.user-nav');
 const mainNavigation = document.querySelector('.main-nav');
+
+if (userNavigation.classList.contains('user-nav--no-js')) {
+  userNavigation.classList.remove('user-nav--no-js');
+  if (mainNavigation.classList.contains('main-nav--no-js')) {
+  mainNavigation.classList.remove('main-nav--no-js');
+}};
 
 navigationToggle.addEventListener('click', function (evn) {
   evn.preventDefault();
